b108e16fdfa66551b8091915d58a24bc13a8aae1
Damon Ramirez <damon.ramirez@tcu.edu>
2020-09-01 20:42:32 -0500

    Initial commit

---
+++ src/main/java/edu/tcu/cs/hw1problem5/Hw1problem5Application.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hw1problem5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw1problem5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw1problem5Application.class, args);
+    }
+
+}
